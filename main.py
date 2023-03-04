3aced2213e6068208160bf6c7abbb3fb61db0f08
CLiz17 <liz362002@gmail.com>
2023-03-05 03:40:30 +0530

    add create-load keys

--- main.py
+++ main.py
@@ -12,17 +12,36 @@ class PasswordManager:
         with open(path, 'wb')as f:
             f.write(self.key)
 
-    def lead_key(self, path):
+    def load_key(self, path):
         with open(path, 'rb') as f:
             self.key = f.read()
 
-    def create_password_file(self, path, initial_value=None):
-        self.password_file = path
-
-        if initial_value is not None:
-            for key, value in initial_value():
-                pass
-
 
 pm = PasswordManager()
-pm.create_key("mykey.key")
+
+print("""What do you want to do?
+(1) Create a new key
+(2) Load an existing key
+(3) Create new password file
+(4) Load existing password file
+(5) Add a new password
+(6) Get a password
+(q) Quit
+""")
+
+done = False
+
+while not done:
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        path = input("Enter path: ")
+        pm.create_key(path)
+    elif choice == "2":
+        path = input("Enter path: ")
+        pm.load_key(path)
+    elif choice == "q":
+        done = True
+        print("Bye")
+    else:
+        print("Invalid choice!")
