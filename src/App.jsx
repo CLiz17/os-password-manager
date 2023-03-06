9ae4766f1b27ff22d49703d5c1cf182814aedb0e
CLiz17 <liz362002@gmail.com>
2023-03-06 17:18:27 +0530

    rm files

--- src/App.jsx
+++ src/App.jsx
@@ -6,50 +6,7 @@ import { TextInput } from 'flowbite-react'
 import PasswordGenerator from './PasswordGenerator'
 
 function App() {
-  const [ website, setWebsite ] = useState("");
-  const [ password, setPassword ] = useState("");
-  const [ card, setCard] = useState([]);
-
-  console.log(website);
-  console.log(password);
-
-  useEffect(() => {
-    getCard();
-  }, [])
-  
-  async function getcard() {
-    try {
-      const { data, error } = await supabase
-        .from("card")
-        .select("*")
-        .limit(10)
-      if (error) throw error;
-      if (data != null) {
-        setcard(data); // [product1,product2,product3]
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-
-  async function createCard() {
-    try {
-      const { data, error } = await supabase
-        .from("card")
-        .insert({
-          website: website,
-          password: password
-        })
-        .single()
-        
-      if (error) throw error;
-      window.location.reload();
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-
-  console.log(card);
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -64,34 +21,24 @@ function App() {
           id="website-url"
           type="website-url"
           placeholder="Website url"
-          onChange={(e) => setWebiste(e.target.value)}
           required={true}/>
       </div>
-      
+
       <div className="m-4 block">
         <Label
-          htmlFor="password"
-          value="password" />
+          htmlFor="username"
+          value="Username" />
         <TextInput
-          id="password"
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
+          id="username"
+          type="username"
+          placeholder="Username"
           required={true}/>
       </div>
-      
+
       <div className="m-4">
         <PasswordGenerator></PasswordGenerator>
-        <Button  onClick={() => createProduct()} className="mt-4" color="success">Save</Button>
+        <Button className="mt-4" color="success">Save</Button>
       </div>
-      <h3>Current Database Password</h3>
-        <Row>
-          {card.map((card) => (
-            <Col>
-              <PasswordCard card={card} /> {}
-            </Col>
-          ))}
-        </Row>
     </div>
   )
 }
